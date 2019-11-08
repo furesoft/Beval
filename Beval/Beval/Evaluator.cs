e0599c8567e49bca86f78a81806448649e462f1a
Furesoft <filmee24@gmail.com>
2019-11-08 17:44:26 +0100

    added simple evaluator

---
+++ Beval/Beval/Evaluator.cs
@@ -0,0 +1,22 @@
+﻿using Loyc;
+using Loyc.Syntax;
+using System;
+using System.Collections.Generic;
+
+namespace Beval
+{
+    internal class Evaluator
+    {
+        public Dictionary<Symbol, Symbol> Aliases { get; set; } = new Dictionary<Symbol, Symbol>();
+
+        public void FindMetadata(LNode ast)
+        {
+            FindAliases(ast);
+        }
+
+        private void FindAliases(LNode ast)
+        {
+            var lines = ast.Args;
+        }
+    }
+}
