6c9b1051d2e1ccfb6efb3d7369c4a8657346b835
Furesoft <filmee24@gmail.com>
2019-11-08 17:43:39 +0100

    added ctor to bevalbool

--- Beval/Beval/ValueTypes/BevalBool.cs
+++ Beval/Beval/ValueTypes/BevalBool.cs
@@ -1,7 +1,12 @@
-﻿namespace Beval
+﻿namespace Beval.ValueTypes
 {
     internal class BevalBool : BevalValue
     {
         public bool Value { get; set; }
+
+        public BevalBool(bool v)
+        {
+            Value = v;
+        }
     }
 }
