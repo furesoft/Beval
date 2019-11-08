52fc957ab85e560eca8359b1d6e38fa2755f1c91
Furesoft <filmee24@gmail.com>
2019-11-08 17:12:39 +0100

    added scope

---
+++ Beval/Beval/BevalScope.cs
@@ -0,0 +1,25 @@
+﻿using Loyc;
+using System.Collections.Generic;
+
+namespace Beval
+{
+    internal class BevalScope
+    {
+        public Dictionary<Symbol, BevalValue> Variables { get; set; }
+
+        public BevalScope(Dictionary<Symbol, BevalValue> variables)
+        {
+            Variables = variables;
+        }
+
+        public static BevalScope Create()
+        {
+            return new BevalScope(new Dictionary<Symbol, BevalValue>());
+        }
+
+        public static BevalScope Create(Dictionary<Symbol, BevalValue> variables)
+        {
+            return new BevalScope(variables);
+        }
+    }
+}
