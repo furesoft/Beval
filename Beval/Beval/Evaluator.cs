b472c3aa00e34604df6341e59cc9383afc552e56
Furesoft <filmee24@gmail.com>
2019-11-09 10:19:18 +0100

    added buildins to evaluator

--- Beval/Beval/Evaluator.cs
+++ Beval/Beval/Evaluator.cs
@@ -1,4 +1,6 @@
-﻿using Loyc;
+﻿using Beval.BuildIns;
+using Beval.ValueTypes;
+using Loyc;
 using Loyc.Syntax;
 using System;
 using System.Collections.Generic;
@@ -8,6 +10,19 @@ namespace Beval
     internal class Evaluator
     {
         public Dictionary<Symbol, Symbol> Aliases { get; set; } = new Dictionary<Symbol, Symbol>();
+
+        public Dictionary<Symbol, BevalFunction> Functions { get; set; } = new Dictionary<Symbol, BevalFunction>();
+
+        public Evaluator()
+        {
+            Functions.Add((Symbol)"not", new NotFunction());
+            Functions.Add((Symbol)"and", new AndFunction());
+            Functions.Add((Symbol)"or", new OrFunction());
+            Functions.Add((Symbol)"xor", new XOrFunction());
+
+            Functions.Add((Symbol)"nand", new NandFunction());
+            Functions.Add((Symbol)"nor", new NOrFunction());
+        }
 
         public void FindMetadata(LNode ast)
         {
