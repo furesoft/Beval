9ec5c13cfe5c11d067cd8d0e274f9e4cec9dca17
Furesoft <filmee24@gmail.com>
2019-11-09 10:15:07 +0100

    added core negated functions

---
+++ Beval/Beval/BuildIns/NorFunction.cs
@@ -0,0 +1,40 @@
+﻿using Beval.ValueTypes;
+using Loyc;
+using System.Linq;
+
+namespace Beval.BuildIns
+{
+    internal class NOrFunction : BevalFunction
+    {
+        public NOrFunction() : base((Symbol)"nor")
+        {
+        }
+
+        public override BevalBool Invoke(params BevalBool[] args)
+        {
+            if (args.Length == 2)
+            {
+                var first = args.First();
+                var second = args.ElementAt(1);
+                bool result = false;
+
+                if (first || second)
+                {
+                    result = true;
+                }
+                else if (first && second)
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+
+                return !result;
+            }
+
+            return null;
+        }
+    }
+}
